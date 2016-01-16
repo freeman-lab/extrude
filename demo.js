0a54e086eb8da9c5d3e977cea6c10061ac7683ec
freeman <the.freeman.lab@gmail.com>
2016-01-16 01:48:55 -0500

    Tweaks to demo

--- demo.js
+++ demo.js
@@ -13,74 +13,139 @@ var time = require('right-now')
 var normals = require('normals')
 var css = require('dom-css')
 var extrude = require('./index.js')
+var soundcloud = require('soundcloud-badge')
+var safari = require('is-safari')
+var mobile = require('is-mobile')
+var Analyser = require('web-audio-analyser')
 
 var canvas = document.body.appendChild(document.createElement('canvas'))
 css(canvas, {zIndex: -1000})
+var gl = context(canvas, render)
+
+if (!mobile() & !safari) {
+  var audio = new Audio()
+  audio.crossOrigin = 'Anonymous'
+  var analyser = Analyser(audio)
+
+  soundcloud({
+    client_id: 'cc4fb3b1e4b84004455321ad04a16580', 
+    song: 'https://soundcloud.com/constellation-records/cst025_track05', 
+    dark: false, 
+    getFonts: true
+  }, function(err, src, data, div) {
+    if (err) throw err
+    audio.src = src
+    audio.loop = true
+    audio.addEventListener('canplay', function() {
+      audio.play()
+    }, false)
+  })
+}
 
 var camera = orbit(canvas)
-var gl = context(canvas, render)
 
 var shapes = ['triangle', 'square', 'hexagon', 'circle', 'heart']
 var options = document.body.appendChild(document.createElement('div'))
-css(options, {position: 'absolute', width: '15%', right: '3%', top: '3%'})
+css(options, {position: 'absolute', right: '6%', top: '2%'})
+
+var link = document.body.appendChild(document.createElement('div'))
+css(link, {position: 'absolute', right: '6%', bottom: '4.5%', textDecoration: 'none'})
+link.innerHTML = 'github'
+link.addEventListener('click', function () {
+  window.location.href = 'http://github.com/freeman-lab/extrude'
+})
+var type = {
+  fontFamily: 'Glacial Indifference', 
+  borderBottom: 'solid 3px rgb(20,20,20)', 
+  borderLeft: 'solid 3px rgba(0, 0, 0, 0)',
+  paddingBottom: 2,
+  paddingLeft: 6,
+  transition: '0.15s',
+  width: '100%',
+  cursor: 'pointer'
+}
+css(link, type)
+mouseover(link)
+mouseout(link)
+
+function mouseover(el) {
+  el.addEventListener('mouseover', function () {
+    css(el, {borderLeft: 'solid 3px rgb(20,20,20)'})
+  })
+}
+
+function mouseout(el) {
+  el.addEventListener('mouseout', function () {
+    css(el, {borderLeft: 'solid 3px rgba(0, 0, 0, 0)'})
+  })
+}
 
 var items = []
 shapes.forEach(function (shape, i) {
   items[i] = options.appendChild(document.createElement('div'))
   items[i].innerHTML = shape
-  css(items[i], {
-    position: 'relative',
-    right: 0, top: 0,
-    fontSize: window.innerWidth * 0.04,
-    marginBottom: window.innerWidth * 0.02,
-    textAlign: 'left', 
-    borderBottom: 'solid 3px black',
-    width: '100%'
-  })
-  window.addEventListener('resize', function () {
-    css(items[i], {fontSize: window.innerWidth * 0.04})
+  css(items[i], type)
+  mouseover(items[i])
+  mouseout(items[i])
+  items[i].addEventListener('click', function () {
+    selection = i
+    reload()
   })
 })
 
+function resize() {
+  var w = Math.sqrt(window.innerWidth * 20)
+  var s = Math.sqrt(window.innerWidth * 1.2)
+  var m = window.innerWidth * 0.01
+  css(options, {width: w})
+  css(link, {width: w, fontSize: s})
+  items.forEach(function (item) {
+    css(item, {fontSize: s, marginBottom: m})
+  })
+}
+
+resize()
+window.addEventListener('resize', resize, false)
 window.addEventListener('resize', fit(canvas), false)
 
 camera.lookAt([3, 3, 4], [0, 0, 0], [1, 0, 0])
 
 var selection = 2
-var shape
+var flattened, geometry, shape
 
-if (selection === 0) {
-  shape = require('./shapes/triangle.js')
+reload()
+
+function reload() {
+  if (selection === 0) {
+    shape = require('./shapes/triangle.js')
+  }
+
+  if (selection === 1) {
+    shape = require('./shapes/square.js')
+  }
+
+  if (selection === 2) {
+    shape = require('./shapes/hexagon.js')
+  }
+
+  if (selection === 3) {
+    shape = require('./shapes/circle.js')
+  }
+
+  if (selection === 4) {
+    shape = require('./shapes/heart.js')
+  }
+
+  var complex = extrude(shape.points, {top: 0.5, bottom: -0.5, closed: true})
+  
+  geometry = Geometry(gl)
+  var flattened = unindex(complex.positions, complex.cells)
+  complex = reindex(flattened)
+  complex.normals = normals.vertexNormals(complex.cells, complex.positions)
+  geometry.attr('position', complex.positions)
+  geometry.attr('normal', complex.normals)
+  geometry.faces(complex.cells)
 }
-
-if (selection === 1) {
-  shape = require('./shapes/square.js')
-}
-
-if (selection === 2) {
-  shape = require('./shapes/hexagon.js')
-}
-
-if (selection === 3) {
-  shape = require('./shapes/circle.js')
-}
-
-if (selection === 4) {
-  shape = require('./shapes/heart.js')
-}
-
-console.log(shape)
-
-var complex = extrude(shape.points, {top: 0.5, bottom: -0.5, closed: true})
-
-var geometry = Geometry(gl)
-
-var flattened = unindex(complex.positions, complex.cells)
-complex = reindex(flattened)
-complex.normals = normals.vertexNormals(complex.cells, complex.positions)
-geometry.attr('position', complex.positions)
-geometry.attr('normal', complex.normals)
-geometry.faces(complex.cells)
 
 var shader = Shader(gl,
   glslify('./shaders/demo.vert'),
@@ -91,6 +156,8 @@ var projection = mat4.create()
 var view = mat4.create()
 
 var background = vignette(gl)
+
+var rotate = 0.005
 
 function render () {
   var width = gl.drawingBufferWidth
@@ -105,7 +172,11 @@ function render () {
   var far = 1000
   mat4.perspective(projection, fov, aspect, near, far)
 
-  camera.rotate([0, 0, 0], [axis * 0.005, -0.005, 0])
+  if (!mobile() & !safari) {
+    rotate = analyser.frequencies().reduce(function (x, y) {return x + y}) / 1000000
+  }
+
+  camera.rotate([0, 0, 0], [axis * rotate, -rotate, 0])
 
   camera.view(view)
   camera.tick()
@@ -118,7 +189,7 @@ function render () {
     scale: [width * 0.0009, height * 0.0009],
     smoothing: [-0.4, 0.6],
     aspect: aspect,
-    color1: [1, 1, 1],
+    color1: [0.95, 0.95, 0.95],
     color2: shape.colors[0],
     coloredNoise: false,
     noiseAlpha: 0.2,
